237a8016a4fbcad88a1d652e7caf0886060bf00d
Towsti <bastiaanmetz+1@gmail.com>
2021-11-07 18:40:33 +0100

    added copy to clipboard functionality and better caret placement

--- site/js/application.js
+++ site/js/application.js
@@ -1,12 +1,31 @@
 const discordEmojiRegex = new RegExp("<:([^:]{2,}):([0-9]+)>", 'i');
 
+function reverse(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
 
 $(document).ready(function() {
     let textInput = document.getElementById('input');
     let pOutput = document.getElementById('output');
 
+//    $("#input").asuggest(emojiSuggestions, {
+//        'autoComplete': false,
+//        'cycleOnTab': true,
+//        'ignoreCase': true,
+//        'endingSymbols': '',
+//        'stopSuggestionKeys': [$.asuggestKeys.RETURN],
+//        'delimiters': '\n ' // ideally not ' ' but requires modification to asuggest
+//    });
+
+    // handle input text area changes
     $('#input').on('input propertychange paste', function() {
         let input = textInput.value;
+        const originalInput = input;
 
         // replace input text: '->' to '→'
         input = input.replace('->', '→');
@@ -29,5 +48,32 @@ $(document).ready(function() {
         // update input and output text
         textInput.value = input;
         pOutput.innerHTML = output;
+
+        // set caret to last edited item instead of always at the end of the text area
+        // e.g. when modifying "hello bye" to "hello -> bye" the caret ends at "hello →| bye"
+        inputReversed = reverse(input);
+        originalInputReversed = reverse(originalInput);
+        for (let i=0; i < inputReversed.length; i++){
+            if (inputReversed[i] != originalInputReversed[i]) {
+                textInput.selectionEnd = input.length - i;
+                break;
+            }
+        }
+    });
+
+    // handle copy button clicked
+    $('#copy').click(function() {
+        let clipboardText = pOutput.innerHTML
+
+        // very pepega way of converting images back to emojis
+        for (const emoji of emojiLUT) {
+            clipboardText = clipboardText.replace(new RegExp('<img class="disc-emoji" src="https://cdn.discordapp.com/emojis/' + discordEmojiRegex.exec(emoji[1])[2] + '.png.v=1">', 'g'), emoji[1]);
+        }
+
+        // copy to clipboard
+        navigator.clipboard.writeText(clipboardText);
+
+        var img = pOutput.toDataURL("image/png");
+        document.write('<img src="'+img+'"/>');
     });
 });
--- update_emoji_lut.py
+++ update_emoji_lut.py
@@ -13,10 +13,16 @@ def get_emoji_lut():
     return sorted(emoji_lut, key=lambda x: len(x[0]), reverse=True)
 
 
+def get_emoji_suggestions(emoji_lut):
+    return [emoji[0] for emoji in reversed(emoji_lut)]
+
+
 def update_emoji_lut():
     emoji_lut = get_emoji_lut()
+    emoji_suggestions = get_emoji_suggestions(emoji_lut)
+
     with open('site/js/emojiLUT.js', 'w') as f:
-        f.write(f"const emojiLUT={emoji_lut};")
+        f.write(f"const emojiLUT = {emoji_lut};\n\nconst emojiSuggestions = {emoji_suggestions};")
 
 
 if __name__ == '__main__':
